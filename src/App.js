508d92b2defe515ed2a5c760e08e2f4c8fc586e5
Ravishan Balasooriya <ravimuay@gmail.com>
2022-02-04 17:51:06 +0530

    Added AddFavorite component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,31 @@ import './App.css';
 import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
+import AddFavorites from './components/AddFavorites';
 
 function App() {
 
   const [movies, setMovies] = useState([]);
 
   const [searchValue, setSearchValue] = useState('');
+
+  const getMoviesRequest = async (searchValue) => {
+
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=2c0c3957`;
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    if(responseJson.Search){
+      setMovies(responseJson.Search);
+    }
+  };
+
+  useEffect(() => {
+
+      getMoviesRequest(searchValue);
+
+  },[searchValue]);
 
   return (
     <div className="container-fluid movie-app">
