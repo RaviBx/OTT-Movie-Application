f8e3c1cef2759f5e3be393a85a881e0490a3a644
Ravishan Balasooriya <ravimuay@gmail.com>
2022-02-04 17:15:00 +0530

    Added App.css

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -5,6 +5,7 @@ const SearchBox = (props) => {
     return(
 
         <div className="col col-sm-4">
+            
             <input type="text"
              className="form-control"
              value={props.value}
